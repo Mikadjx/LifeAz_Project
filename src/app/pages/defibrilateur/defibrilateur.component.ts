a1c598f0ead7d7efdee44b4c9c11f6c79c4d7e04
Dijoux <chezdijoux@gmail.com>
2022-12-17 23:38:06 +0100

    version3

--- src/app/pages/defibrilateur/defibrilateur.component.ts
+++ src/app/pages/defibrilateur/defibrilateur.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
+
+
+// Paramétre du composant defibrilateurComponent
 
 @Component({
   selector: 'app-defibrilateur',
@@ -7,40 +11,150 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./defibrilateur.component.css']
 })
 
-// variable permettant de de dynamiser le html 
 
-export class DefibrilateurComponent implements OnInit  {
+export class DefibrilateurComponent implements OnInit {
 
-  public logo_lifeaz ="assets/LIFEAZ.png"
-  public logo_menu ="assets/icon_menu.png"
-  public logo_check="assets/icon_check.png"
-  public logo_danger ="assets/icon_danger.png"
-  public logo_inter ="assets/icon_inter.png"
-  public logo_spark="assets/icon_spark.png"
-  public logo_loc="assets/icon_loc.png"
-  public logo_line2="assets/Line 2.png"
 
-  //variable de notre base de données LifeAz 
+  // Variable Image 
+
+  public logo_lifeaz = "assets/LIFEAZ.png"
+  public logo_menu = "assets/icon_menu.png"
+  public logo_check = "assets/icon_check.png"
+  public logo_danger = "assets/icon_danger.png"
+  public logo_inter = "assets/icon_inter.png"
+  public logo_spark = "assets/icon_spark.png"
+  public logo_loc = "assets/icon_loc.png"
+  public logo_line2 = "assets/Line 2.png"
+
+  // Variable Texte 
   
-  public monTitre : string;
-  public Serial : any;
-  public locationName: any;
-  public locationAddr: any;
-  public state: any;
-  public electrodesexpiry: any;
 
-  constructor( http: HttpClient){
-    this.monTitre = "MES DÉFIBRILATEURS";
+  public Title : string;
+ defibrilator : any;
+ serial:any;
+ locatioName: any;
+ locationAddr:any;
+ state:any;
+ electrodesExpiry:any;
+ 
+
+
+
+  
+constructor(public http: HttpClient) {
+ this.Title = "MES DEFIBRILATEURS";
+
+ 
+
   }
 
-  ngOnInit(): void{
-  
+  ngOnInit():void{
+    this.http.get<any>('http://localhost:5500/defibrilator')
+     .subscribe(response => {
+      console.log(response[0]);
+      this.defibrilator = response[0];
+    
+      console.log(response[1]);
+      
+
+
+
+    
+     });
   }
-
-
-
 }
 
 
 
 
+ 
+ 
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*export class DefibrilateurComponent implements OnInit {
+
+
+  // Image 
+  public logo_lifeaz = "assets/LIFEAZ.png"
+  public logo_menu = "assets/icon_menu.png"
+  public logo_check = "assets/icon_check.png"
+  public logo_danger = "assets/icon_danger.png"
+  public logo_inter = "assets/icon_inter.png"
+  public logo_spark = "assets/icon_spark.png"
+  public logo_loc = "assets/icon_loc.png"
+  public logo_line2 = "assets/Line 2.png"
+
+  // initialiser nos valeurs à apparaitre sur le html 
+  ngOnInit():void{
+
+  }
+
+  //Texte 
+
+  public Title : string;
+  public Menu : string;
+   defibrilatorApiUrl = '';
+  
+   defibrilatorData = {
+   
+    Serial: '',
+    locationName: '',
+    locationAddr: '',
+    state: '',
+   electrodesExpiry: ''
+   
+  };
+
+
+
+  //variable de notre base de données LifeAz 
+/*
+  constructor(public http: HttpClient) {
+
+    this.Title = "Mes défibrilateurs";
+    this.Menu = "MENU";
+
+
+    this.defibrilatorApiUrl = 'http://localhost:5500/defibrilator';
+
+
+this.readAPI(this.defibrilatorApiUrl) .subscribe((data) => { console.log(data);
+  this.defibrilatorData. Serial = data['serial'];
+  this.defibrilatorData.locationName = data['locationName'];
+  this.defibrilatorData. locationAddr= data['locationAddr'];
+  this.defibrilatorData. state= data['state'];
+  this.defibrilatorData. electrodesExpiry= data['electrodesExpiry'];*/
